54b33503fffbfb79df4542502cdd88cfb16994e5
Inceptors-hackathon <inceptors.hackathon@gmail.com>
2020-09-27 04:17:47 +0530

    3rd commit

--- django/django_project/hp/views.py
+++ django/django_project/hp/views.py
@@ -5,3 +5,4 @@ from django.http import HttpResponse
 
 def home(request): 
     return HttpResponse('<h1> Home Page </h1>')
+    return HttpResponse('<!DOCTYPE html><html><head><link rel="stylesheet" href="Untitled-1.css"></head><body><h1 id="first">DISPLAY</h1><h1 id="second">DISPLAY2</h1></body></html>')
